8eab897e7b3d1671dbd311a7418a7ca71260e795
Dylan Sprague <dylan.richard.sprague@gmail.com>
2016-12-13 20:27:47 -0500

    Solve exercise 1.2
    Take two integer command-line arguments, add them, print the result

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,14 @@ module Main where
 
 import System.Environment
 
+addTwoStrings :: String -> String -> String
+addTwoStrings a b = show $ read a + read b
+
 main :: IO ()
 main = do
      args <- getArgs
-     putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
+     let arg0 = args !! 0
+     let arg1 = args !! 1
+     putStrLn (arg0 ++ " + " ++ arg1 ++ " = " ++ addTwoStrings arg0 arg1)
+     where 
+        
