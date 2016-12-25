7ea6f66c970e2c37901c821d0dd5109ed7c3553d
Dylan Sprague <dylan.richard.sprague@gmail.com>
2016-12-24 19:59:42 -0500

    Add basic evaluation

--- src/Main.hs
+++ src/Main.hs
@@ -87,13 +87,16 @@ showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList listHead listTail) = "(" ++ unwordsList listHead ++ " . " ++ showVal listTail ++ ")"
 
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found " ++ show val
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
 
 main :: IO ()
-main = do 
-        (expr:_) <- getArgs
-        putStrLn (readExpr expr)
+main = getArgs >>= print . eval . readExpr . head
