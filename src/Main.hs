61795965f11a0bd41f3bf5d6399845ead1fb8748
Dylan Sprague <dylan.richard.sprague@gmail.com>
2017-01-01 17:14:38 -0500

    Add REPL

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad
 import Control.Monad.Error
+import System.IO
 
 data LispVal = Atom String
              | List [LispVal]
@@ -268,15 +269,39 @@ extractValue :: ThrowsError a -> a
 extractValue (Left _) = undefined
 extractValue (Right val) = val
 
---main loop
+--main loop with helper functions
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
     Left err -> throwError $ Parser err
     Right val -> return val
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr =  evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ predicate prompt action = do 
+   result <- prompt
+   if predicate result 
+      then return ()
+      else action result >> until_ predicate prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
 main = do
     args <- getArgs
-    evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-    putStrLn $ extractValue $ trapError evaled
+    case length args of
+        0 -> runRepl
+        1 -> evalAndPrint $ args !! 0
+        _ -> putStrLn "Program takes only 0 or 1 argument"
